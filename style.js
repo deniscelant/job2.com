6a40b324a93f629668f02d19311593ebe02f3a28
deniscelant <dedecelant123@gmail.com>
2025-01-15 12:48:43 -0300

    main

---
+++ style.js
@@ -0,0 +1,13 @@
+export const purple = "rgb(41, 41, 206)"
+export const outline = "solid 1px rgb(41, 41, 206)"
+
+// export function listMarked(element){
+//     element.style.backgroundColor = "white";
+//     element.style.color = purple;
+//     element.style.border = outline;
+// }  
+
+// export function listUnmarked(element){
+//     element.style.backgroundColor = purple;
+//     element.style.color = "white";
+// }  
